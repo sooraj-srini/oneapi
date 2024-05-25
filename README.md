9d93b171c96671d202560b5462d8d9b29604d444
sooraj-srini <cs20b075@smail.iitm.ac.in>
2024-05-25 13:43:59 +0530

    simplified

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ Doubts I have with sycl still:
 - memory management is kinda weird but i trust sycl now at least
 - i hate the fact that i can't use two parallel_for in one kernel?? what is the point then? I can't even reuse the ever growing accessors??
 - its not that bad.
+
+# why is sycl
+
+copilot tells me that "However, the key difference between SYCL and CUDA is that SYCL does not automatically synchronize between operations. In CUDA, operations in the same stream are guaranteed to execute in order, and a kernel will not start executing until all previous operations in the same stream have completed. In SYCL, if you want to ensure that a memcpy operation completes before a kernel starts, you need to use sycl::queue::wait() to explicitly wait for the memcpy operation to complete before submitting the kernel. "
+
+In what world are the operations in the same stream then? The concept of a stream is that the operations have a global barrier.
+
+so essentially, using usm is shooting yourself into your foot. I don't know why you would want to do it then, unless you just don't want to have the data on your host memory for some reason.  
+
+holy shit. I think the expected way to sync between memcpy and a kernel without using cds is to have a hh.depends_on(memcpy) command in the kernel command group. why is it so cringe.
--- a3.cpp
+++ a3.cpp
@@ -1,20 +1,23 @@
-#include <algorithm>
 #include <vector>
-#include <CL/sycl.hpp>
+#include <sycl/sycl.hpp>
+
+using namespace sycl;
 
 int main()
 {
 
     // take in input from stdin
+    std::ios::sync_with_stdio(false);
 
     int num_meshes, R, C;
     std::cin >> num_meshes >> R >> C;
-    std::vector<int> x(num_meshes), y(num_meshes), sx(num_meshes), sy(num_meshes), opacity(num_meshes);
+    std::vector<int> sx(num_meshes), sy(num_meshes), opacity(num_meshes);
+    std::vector<std::pair<int, int>> x(num_meshes); 
     std::vector<std::vector<int>> mesh(num_meshes);
 
     for (int i = 0; i < num_meshes; i++)
     {
-        std::cin >> sx[i] >> sy[i] >> x[i] >> y[i] >> opacity[i];
+        std::cin >> sx[i] >> sy[i] >> x[i].first >> x[i].second >> opacity[i];
         mesh[i].resize(sx[i] * sy[i]);
         for (int j = 0; j < sx[i] * sy[i]; j++)
         {
@@ -45,45 +48,45 @@ int main()
 
     int num_trans;
     std::cin >> num_trans;
-    std::vector<int> up(num_meshes), right(num_meshes);
+    std::vector<std::pair<int, int>> up(num_meshes, {0, 0});
     for (int i = 0; i < num_trans; i++)
     {
         int v, c, a;
         std::cin >> v >> c >> a;
         if (c == 0)
-            up[v] -= a;
+            up[v].first -= a;
         else if (c == 1)
-            up[v] += a;
+            up[v].first += a;
         else if (c == 2)
-            right[v] -= a;
+            up[v].second -= a;
         else if (c == 3)
-            right[v] += a;
+            up[v].second += a;
     }
-    std::vector<int> cum_up(up), cum_right(right);
+    std::vector<std::pair<int, int>> cum_up(up);
 
     // input taken in hopefully
 
     // we need to do dfs on the tree and update the transformations of each node
     std::vector<int> parent(num_meshes, -1);
     std::vector<int> grandparent(num_meshes, -1);
-    std::vector<int> grid(R*C, 0);
+    std::vector<int64_t> grid(R*C, 0);
 	auto start = std::chrono::high_resolution_clock::now () ;
-    sycl::queue h;
+    queue h;
 
     {
-        sycl::buffer hOffset_buf(hOffset), hCsr_buf(hCsr), up_buf(up), right_buf(right), x_buf(x), y_buf(y), sx_buf(sx), sy_buf(sy), opacity_buf(opacity);
-        sycl::buffer parent_buf(parent), grandparent_buf(grandparent);
-        sycl::buffer cum_up_buf(cum_up), cum_right_buf(cum_right);
+        buffer hOffset_buf(hOffset), hCsr_buf(hCsr), sx_buf(sx), sy_buf(sy), opacity_buf(opacity);
+        buffer parent_buf(parent), grandparent_buf(grandparent);
+        buffer cum_up_buf(cum_up), up_buf(up), x_buf(x);
 
-        h.submit([&](sycl::handler &hh)
+        h.submit([&](handler &hh)
                  {
-                     sycl::accessor hOffset_acc(hOffset_buf, hh, sycl::read_only);
-                     sycl::accessor hCsr_acc(hCsr_buf, hh, sycl::read_only);
-                     sycl::accessor parent_acc(parent_buf, hh, sycl::write_only);
+                     accessor hOffset_acc(hOffset_buf, hh, read_only);
+                     accessor hCsr_acc(hCsr_buf, hh, read_only);
+                     accessor parent_acc(parent_buf, hh, write_only);
 
                      // update parent of node in parent accessor
 
-                     hh.parallel_for(num_meshes, [=](sycl::id<1> i)
+                     hh.parallel_for(num_meshes, [=](id<1> i)
                                      {
                             int start = hOffset_acc[i];
                             int end = hOffset_acc[i + 1];
@@ -96,28 +99,23 @@ int main()
         for (int i = 0; i < updates; i++)
         {
 
-            h.submit([&](sycl::handler &hh){
-                sycl::accessor parent_acc(parent_buf, hh, sycl::read_write);
-                sycl::accessor grand_acc(grandparent_buf, hh, sycl::read_write);
+            h.submit([&](handler &hh){
+                accessor parent_acc(parent_buf, hh, read_write);
+                accessor grand_acc(grandparent_buf, hh, read_write);
                 if(i% 2 == 1) {
-                    // auto tmp = parent_acc;
-                    // parent_acc = grand_acc;
-                    // grand_acc = tmp;
                     std::swap(parent_acc, grand_acc);
                 }
-                sycl::accessor up_acc(up_buf, hh, sycl::read_only);
-                sycl::accessor right_acc(right_buf, hh, sycl::read_only);
-                sycl::accessor cum_up_acc(cum_up_buf, hh, sycl::read_write);
-                sycl::accessor cum_right_acc(cum_right_buf, hh, sycl::read_write);
+                accessor up_acc(up_buf, hh, read_only);
+                accessor cum_up_acc(cum_up_buf, hh, read_write);
 
                  // update parent of node in parent accessor
 
-                hh.parallel_for(num_meshes, [=](sycl::id<1> i)
+                hh.parallel_for(num_meshes, [=](id<1> i)
                     {
                     int par = parent_acc[i];
                     if(par >= 0) {
-                        cum_up_acc[i] = cum_up_acc[i] + up_acc[par];
-                        cum_right_acc[i] = cum_right_acc[i] + right_acc[par];
+                        cum_up_acc[i].first = cum_up_acc[i].first + up_acc[par].first;
+                        cum_up_acc[i].second = cum_up_acc[i].second + up_acc[par].second;
                         grand_acc[i] = parent_acc[par];
                     } else {
                         grand_acc[i] = -1;
@@ -126,14 +124,11 @@ int main()
                 ); 
             });
 
-            h.submit([&](sycl::handler &hh) {
-                sycl::accessor up_acc (up_buf, hh, sycl::write_only);
-                sycl::accessor right_acc (right_buf, hh, sycl::write_only);
-                sycl::accessor cum_right_acc (cum_right_buf, hh, sycl::read_only);
-                sycl::accessor cum_up_acc (cum_up_buf, hh, sycl::read_only);
-                hh.parallel_for(num_meshes, [=](sycl::id<1> i) {
+            h.submit([&](handler &hh) {
+                accessor up_acc (up_buf, hh, write_only);
+                accessor cum_up_acc (cum_up_buf, hh, read_only);
+                hh.parallel_for(num_meshes, [=](id<1> i) {
                     up_acc[i] = cum_up_acc[i];
-                    right_acc[i] = cum_right_acc[i];
                 });
             });
 
@@ -151,24 +146,19 @@ int main()
         for(int i=0; i<V; i++){
             std::copy(mesh[i].begin(), mesh[i].end(), meshall.begin() + offset[i]);
         }
-        sycl::buffer mesh_buf(meshall), offset_buf(offset);
-        std::vector<int> lock(R*C, 0);
-        sycl::buffer lock_buf(lock); 
-        sycl::buffer grid_buf(grid); 
-        h.submit([&](sycl::handler &hh) {
-            sycl::accessor mesh_acc(mesh_buf, hh, sycl::read_only);
-            sycl::accessor offset_acc(offset_buf, hh, sycl::read_only);
-            sycl::accessor cum_up_acc(cum_up_buf, hh, sycl::read_only);
-            sycl::accessor cum_right_acc(cum_right_buf, hh, sycl::read_only);
-            sycl::accessor sx_acc(sx_buf, hh, sycl::read_only);
-            sycl::accessor sy_acc(sy_buf, hh, sycl::read_only);
-            sycl::accessor x_acc(x_buf, hh, sycl::read_only);
-            sycl::accessor y_acc(y_buf, hh, sycl::read_only);
-            sycl::accessor op_acc(opacity_buf, hh, sycl::read_only);
-            sycl::accessor grid_acc(grid_buf, hh, sycl::read_write);
-            sycl::accessor lock_acc(lock_buf, hh, sycl::read_write);
+        buffer mesh_buf(meshall), offset_buf(offset); 
+        buffer grid_buf(grid); 
+        h.submit([&](handler &hh) {
+            accessor mesh_acc(mesh_buf, hh, read_only);
+            accessor offset_acc(offset_buf, hh, read_only);
+            accessor cum_up_acc(cum_up_buf, hh, read_only);
+            accessor sx_acc(sx_buf, hh, read_only);
+            accessor sy_acc(sy_buf, hh, read_only);
+            accessor x_acc(x_buf, hh, read_only);
+            accessor op_acc(opacity_buf, hh, read_only);
+            accessor grid_acc(grid_buf, hh, read_write);
 
-            hh.parallel_for(total, [=](sycl::id<1> i) {
+            hh.parallel_for(total, [=](id<1> i) {
                 int tid = i[0];
                 int left = 0, right = V;
                 while (left < right) {
@@ -188,39 +178,31 @@ int main()
                 int off = tid - offset_acc[vertex];
                 int r =  off /sy_acc[vertex];
                 int c = off % sy_acc[vertex];
-                if (vertex < V && r < sx_acc[vertex] && c < sy_acc[vertex]) {
-                    int ux = x_acc[vertex] + r + cum_up_acc[vertex];
-                    int uy = y_acc[vertex] + c + cum_right_acc[vertex];
+                int ux = x_acc[vertex].first + r + cum_up_acc[vertex].first;
+                int uy = x_acc[vertex].second + c + cum_up_acc[vertex].second;
 
-                    if (ux >= 0 && ux < R && uy >= 0 && uy < C) {
-                        int idx = ux * C + uy;
-                        int op = op_acc[vertex];
-                        bool done = false, updated = false;
-                        sycl::atomic_ref<int, sycl::memory_order::relaxed, sycl::memory_scope::system, sycl::access::address_space::global_space> lock_ref(lock_acc[idx]);
-                        do {
-                            done = updated;
-                            int old = lock_ref;
-                            bool val = !(old >= 0 && old < op);
-                            int exp = old - INT_MAX*val;
-                            int des = -1;
-                            bool suc = lock_ref.compare_exchange_strong(exp, des);
-
-                            if(suc) {
-                                grid_acc[idx] = mesh_acc[offset_acc[vertex] + r*sy_acc[vertex] + c];
-                                lock_ref = op;
-                                updated = true;
-                            }
-                            updated = updated | (old >= op);
-
-                        } while(!done);
-                    }
-                    
+                if (ux >= 0 && ux < R && uy >= 0 && uy < C) {
+                    int idx = ux * C + uy;
+                    atomic_ref<int64_t, memory_order::relaxed, memory_scope::system, access::address_space::global_space> lock_ref(grid_acc[idx]);
+                    int64_t op = op_acc[vertex];
+                    int64_t val = mesh_acc[offset_acc[vertex] + r*sy_acc[vertex] + c];
+                    int64_t both = (op << 32) | val;
+                    lock_ref.fetch_max(both);
                 }
+                    
             });
 
         });
 
-        h.wait();
+        h.submit([&](handler &hh) {
+            accessor grid_acc(grid_buf, hh, read_write);
+            hh.parallel_for(R*C, [=](id<1> i) {
+                int idx = i[0];
+                int64_t both = grid_acc[idx];
+                int val = both & 0xFFFFFFFF;
+                grid_acc[idx] = val;
+            });
+        });
 
     }
 	auto end  = std::chrono::high_resolution_clock::now () ;
